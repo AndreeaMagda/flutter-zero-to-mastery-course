fb565f8aeaa24ee04dfb4c2510ee8810bf67a559
Magda Andreea <122922300+AndreeaMagda@users.noreply.github.com>
2024-02-24 17:43:56 +0200

    FutureProvider

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,8 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:ztmcourse/providers.dart';
 
 void main() {
-  runApp(const ProviderScope(child: const MyApp()));//allows us to access to all the providers 
+  runApp(const ProviderScope(
+      child: const MyApp())); //allows us to access to all the providers
 }
 
 class MyApp extends StatelessWidget {
@@ -12,8 +13,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-     
-home: MyHomePage(),
+      home: MyHomePage(),
     );
   }
 }
@@ -21,24 +21,28 @@ home: MyHomePage(),
 class MyHomePage extends ConsumerWidget {
   const MyHomePage({super.key});
 
-
   @override
-  Widget build(BuildContext context,WidgetRef ref) {
+  Widget build(BuildContext context, WidgetRef ref) {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        
       ),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
-          children:[
-            Consumer(builder: (context,ref,child){
-             return Text(ref.watch(normalProvider));
-            })
+          children: [
+               Text(ref.watch(normalProvider)),
+               ref.watch(messageProvider).when(data: (message){
+                return Text(message);
+               }, error: ((error, stackTrace) {
+                return const Text("error");
+               } ), loading: (){
+                return const CircularProgressIndicator();
+               })
+           
           ],
         ),
       ),
     );
-  
-}}
+  }
+}
--- lib/providers.dart
+++ lib/providers.dart
@@ -3,3 +3,9 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 final normalProvider = Provider<String>((ref) {
   return "I'm just a normal provider" ;
 });
+
+final messageProvider = FutureProvider<String>((ref) async {
+  return Future.delayed(const Duration(seconds: 5),(){
+    return "A message from the Future";
+  });
+});
