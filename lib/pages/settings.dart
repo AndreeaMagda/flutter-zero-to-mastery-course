25a99295545cde16c73d3df0df8ac5eec02dbcdf
Magda Andreea <122922300+AndreeaMagda@users.noreply.github.com>
2024-03-02 18:12:36 +0200

    Updating User Name

--- lib/pages/settings.dart
+++ lib/pages/settings.dart
@@ -1,12 +1,40 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:ztmcourse/providers/user_provider.dart';
 
-class Settings extends StatelessWidget {
+class Settings extends ConsumerStatefulWidget {
   const Settings({super.key});
+  @override
+  ConsumerState<Settings> createState() => _SettingsState();
+}
 
+class _SettingsState extends ConsumerState<Settings> {
+  TextEditingController _nameController = TextEditingController();
   @override
   Widget build(BuildContext context) {
-    return  Scaffold(
-      appBar: AppBar(title:const Text("Settings Page"),),
+    LocalUser currentUser = ref.watch(userProvider);
+    _nameController.text = currentUser.user.name;
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Settings Page"),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          children: [
+            TextFormField(
+              decoration: const InputDecoration(labelText: "Enter your name"),
+              controller: _nameController,
+            ),
+            TextButton(
+              onPressed: () {
+                ref.read(userProvider.notifier).updateName(_nameController.text);
+              },
+              child: const Text("Update"),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
--- lib/providers/user_provider.dart
+++ lib/providers/user_provider.dart
@@ -26,7 +26,10 @@ class LocalUser {
 
 class UserNotifier extends StateNotifier<LocalUser> {
   UserNotifier()
-      : super(const LocalUser(id: "error", user: FirebaseUser(email: "error", name:"error", profilePic: "error")));
+      : super(const LocalUser(
+            id: "error",
+            user: FirebaseUser(
+                email: "error", name: "error", profilePic: "error")));
 
   final FirebaseFirestore _firestore = FirebaseFirestore.instance;
 
@@ -35,33 +38,44 @@ class UserNotifier extends StateNotifier<LocalUser> {
         .collection("users")
         .where("email", isEqualTo: email)
         .get();
-        if(response.docs.isEmpty){
-          print("No firebase user associated to authenticated email $email");
-          return;
-        }
-          if(response.docs.length!=1){
-          print("More than one firebase user associate with email $email");
-          return;
-        }
+    if (response.docs.isEmpty) {
+      print("No firebase user associated to authenticated email $email");
+      return;
+    }
+    if (response.docs.length != 1) {
+      print("More than one firebase user associate with email $email");
+      return;
+    }
 
     state = LocalUser(
-      id: response.docs[0].id,
-      user:  FirebaseUser.fromMap(response.docs[0].data() as Map<String,dynamic>)
-    );
+        id: response.docs[0].id,
+        user: FirebaseUser.fromMap(
+            response.docs[0].data() as Map<String, dynamic>));
   }
 
   Future<void> signUp(String email) async {
     DocumentReference response = await _firestore.collection("users").add(
-          FirebaseUser(email: email, name: "No name",profilePic: 'http://www.gravatar.com/avatar/?d=mp').toMap(),
+          FirebaseUser(
+                  email: email,
+                  name: "No name",
+                  profilePic: 'http://www.gravatar.com/avatar/?d=mp')
+              .toMap(),
         );
-        DocumentSnapshot snapshot=await response.get();
+    DocumentSnapshot snapshot = await response.get();
     state = LocalUser(
       id: response.id,
-      user: FirebaseUser.fromMap(snapshot.data() as Map<String,dynamic>),
+      user: FirebaseUser.fromMap(snapshot.data() as Map<String, dynamic>),
     );
   }
 
+  Future<void> updateName(String name) async {
+    await _firestore.collection("users").doc(state.id).update({'name': name});
+    state=state.copyWith(user:state.user.copyWith(name:name));
+  }
+
   void logout() {
-    state = const LocalUser(id: "error", user: FirebaseUser(email: "error", name:"error", profilePic: "error"));
+    state = const LocalUser(
+        id: "error",
+        user: FirebaseUser(email: "error", name: "error", profilePic: "error"));
   }
 }
